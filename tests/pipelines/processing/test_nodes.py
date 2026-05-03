80722980c7d247bba86a54fe53fa5184376d7cd1
Dnewe <ewenstvallier@gmail.com>
2026-05-03 20:37:23 +0200

    empty commit for test

--- tests/pipelines/processing/test_nodes.py
+++ tests/pipelines/processing/test_nodes.py
@@ -3,7 +3,6 @@ import numpy as np
 from purchase_predict.pipelines.processing.nodes import encode_features, split_dataset
 from sklearn.preprocessing import LabelEncoder
 
-
 BALANCE_THRESHOLD = 0.1
 MIN_SAMPLES = 5000
 
